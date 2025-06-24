982fa39633114c887fc51f7f2efb2046649a8e75
wabasit <basit.wahab@amalitechtraining.org>
2025-06-24 15:21:43 +0000

    Defining a rental duration hours from rental end time and rental start time in hours

--- scripts/vehicle_location_metrics.py
+++ scripts/vehicle_location_metrics.py
@@ -7,3 +7,8 @@ spark = SparkSession.builder.appName("VehicleLocationMetrics").getOrCreate()
 rental_df = spark.read.csv("s3://rentalbk/raw_data/rental_transactions.csv", header=True, inferSchema=True)
 location_df = spark.read.csv("s3://rentalbk/raw_data/locations.csv", header=True, inferSchema=True)
 vehicle_df = spark.read.csv("s3://rentalbk/raw_data/vehicles.csv", header=True, inferSchema=True)
+
+# Rental duration (in hours)
+rental_df = rental_df.withColumn("rental_duration_hours",
+    (unix_timestamp("rental_end_time") - unix_timestamp("rental_start_time")) / 3600
+)
