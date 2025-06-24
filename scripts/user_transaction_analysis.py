fcbeb2380cd6a4f3c491b887009ac08b27c2e490
wabasit <basit.wahab@amalitechtraining.org>
2025-06-24 15:07:06 +0000

    Converting rental duration to hours

--- scripts/user_transaction_analysis.py
+++ scripts/user_transaction_analysis.py
@@ -6,3 +6,9 @@ spark = SparkSession.builder.appName("UserTransactionAnalysis").getOrCreate()
 # Read datasets
 rental_df = spark.read.csv("s3://rentalbk/raw_data/rental_transactions.csv", header=True, inferSchema=True)
 
+# Rental duration in hours
+rental_df = rental_df.withColumn("rental_duration_hours",
+    (unix_timestamp("rental_end_time") - unix_timestamp("rental_start_time")) / 3600
+)
+
+
